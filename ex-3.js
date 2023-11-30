9b08402ea10af41c5159123e44ecd554eabc608e
PIPOAUMPHOL <aumpholllo@gmail.com>
2023-11-30 18:41:02 +0700

    feat(exercise): add completed exercise

--- ex-3.js
+++ ex-3.js
@@ -1,6 +1,15 @@
 // Exercise #3: Find a Minimum Number
 let numbers = [100, 20, 3, 1000];
-let minNumber;
+let minNumber = numbers[0];
+
 // Start coding here
+
+console.log(minNumber);
+
+for (let i = 1; i < numbers.length; i++) {
+  if (numbers[i] < minNumber) {
+    minNumber = numbers[i];
+  }
+}
 
 console.log(minNumber);
