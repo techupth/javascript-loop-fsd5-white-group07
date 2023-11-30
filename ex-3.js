8d792e876f5fc966b793f2c0548a426f2e56e051
PIPOAUMPHOL <aumpholllo@gmail.com>
2023-11-30 18:45:51 +0700

    fix: change ex-3.js answer to be more simple

--- ex-3.js
+++ ex-3.js
@@ -1,13 +1,13 @@
 // Exercise #3: Find a Minimum Number
 let numbers = [100, 20, 3, 1000];
-let minNumber = numbers[0];
+let minNumber;
 
 // Start coding here
 
 console.log(minNumber);
 
 for (let i = 1; i < numbers.length; i++) {
-  if (numbers[i] < minNumber) {
+  if (numbers[i] < numbers[0]) {
     minNumber = numbers[i];
   }
 }
