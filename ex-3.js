a21666a4d88e89960f84d44aa33dd15351ef5df2
FiveTW87 <thanwalokwattanakul@gmail.com>
2023-12-01 13:53:44 +0700

    feat:complete Ex

--- ex-3.js
+++ ex-3.js
@@ -1,6 +1,28 @@
 // Exercise #3: Find a Minimum Number
-let numbers = [100, 20, 3, 1000];
+let numbers = [10, 100, 20, 3, 1000];
 let minNumber;
+let maxNumber;
 // Start coding here
-
+for (let i = 0; i < numbers.length; i++) {
+    if (i === 0) {
+        minNumber = numbers[i]
+        maxNumber = numbers[i]
+    }  else{
+        if (minNumber > numbers[i]){
+            minNumber = numbers[i]
+        } else if (maxNumber < numbers[i]){
+            maxNumber = numbers[i]
+        }
+}
+}
+//   if (maxNumber < numbers[i]) {
+//     maxNumber = numbers[i];
+//   } else if (minNumber > numbers[i]){
+//     minNumber = numbers[i]
+//   }
+  // if (numbers[i]< numbers[i+1])
+  // minNumber = numbers[i]
+  // else  (numbers[i]>numbers[i+1])
+  // maxNumber = numbers[i]
 console.log(minNumber);
+console.log(maxNumber);
--- ex-5.js
+++ ex-5.js
@@ -2,3 +2,9 @@
 let companyName = "TechUp";
 
 // Start coding here
+for (let i= 0 ; i < companyName.length ; i++){
+    let str = companyName[i]
+    let cha = i + 1
+
+    console.log("Number "+cha+" character is " + str)
+}
